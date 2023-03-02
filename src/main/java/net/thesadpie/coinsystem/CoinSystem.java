1ec0443d53e113657431e34377d1a417714fdac0
Elisa <acrazypie5@gmail.com>
2023-03-02 18:24:42 +0100

    First Commit

---
+++ src/main/java/net/thesadpie/coinsystem/CoinSystem.java
@@ -0,0 +1,16 @@
+package net.thesadpie.coinsystem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoinSystem implements ModInitializer {
+	public static final String MOD_ID = "coinsystem";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/thesadpie/coinsystem/CoinSystemClient.java
@@ -0,0 +1,10 @@
+package net.thesadpie.coinsystem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoinSystemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
